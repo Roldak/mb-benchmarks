6a72242832d6019ab56429623c2e723a0a019771
Romain <romain.beguet@epfl.ch>
2015-03-23 15:19:47 +0100

    preparing things

---
+++ mbarrays/src/Main.scala
@@ -0,0 +1,42 @@
+package miniboxing.example
+
+import MbArray._
+
+object Test {
+  final val len = 20000000
+ 
+  def sumA(ary: Array[Any]) = {
+    for (i <- 1 until len) {
+        ary(i) = 1
+        ary(0) = ary(0).asInstanceOf[Int] + ary(i).asInstanceOf[Int]
+    }
+    ary(0)
+  }
+ 
+  def sumB(ary: MbArray[Int]) = {
+    for (i <- 1 until len) {
+        ary(i) = 1
+        ary(0) = ary(0) + ary(i)
+    }
+    ary(0)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val a = new Array[Any](len)
+    val b = MbArray.empty[Int](len)
+   
+    println(a.getClass())
+    for (i <- 0 until 30) {
+        val startA = System.nanoTime
+        val sA = sumA(a)
+        println(sA + " in " + (System.nanoTime - startA) / 1000000.0 + " milliseconds")
+    }
+   
+    println(b.getClass())
+    for (i <- 0 until 30) {
+        val startB = System.nanoTime
+        val sB = sumB(b)
+        println(sB + " in " + (System.nanoTime - startB) / 1000000.0 + " milliseconds")
+    }
+  }
+}
--- project/Build.scala
+++ project/Build.scala
@@ -3,6 +3,36 @@ import Keys._
 
 object MyBuild extends Build {
 
-  lazy val root = project.in(file("."))
+  scalaVersion in ThisBuild := "2.11.5"
+
+  lazy val root = project.in(file(".")).aggregate(mbarray)
   
+  lazy val mbarray = Project(
+	"mbarray",
+	file("mbarray"),
+    settings = Defaults.defaultSettings ++ Seq[Setting[_]](
+      organization := "ch.epfl.lamp",
+      version := "0.1-SNAPSHOT",
+      // The plugin requires the latest version of the scalac compiler. You
+      // can use older compilers, but before reporting a bug, please check
+      // that it can be reproduced with the latest version of the compiler.
+      scalaVersion := "2.11.5"
+    ) ++ miniboxingSettings
+  )
+  
+  libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value
+  
+  /** Settings for the miniboxing plugin */
+  lazy val miniboxingSettings = Seq[Setting[_]](
+    resolvers += Resolver.sonatypeRepo("snapshots"),
+    libraryDependencies += "org.scala-miniboxing.plugins" %% "miniboxing-runtime" % "0.4-SNAPSHOT",
+    addCompilerPlugin("org.scala-miniboxing.plugins" %% "miniboxing-plugin" % "0.4-SNAPSHOT"),
+    scalacOptions ++= (
+      "-P:minibox:log" ::    // enable the miniboxing plugin output
+      //                       // (which explains what the plugin is doing)
+      //"-P:minibox:hijack" :: // enable hijacking the @specialized annotations
+      //                       // transforming them into @miniboxed annotations
+      Nil
+    )
+  )
 }
